6dc3bfbc0394d4fe1cec73c23e9373b7ef878059
W-X-Z <116233351+W-X-Z@users.noreply.github.com>
2023-10-25 23:07:45 +0900

    반응형

--- stockgame/app.js
+++ stockgame/app.js
@@ -1,6 +1,8 @@
 const CONSTANTS = {
     ORIGINAL_WIDTH: 540,
-    ORIGINAL_HEIGHT: 987,
+    ORIGINAL_HEIGHT: 160,
+    CREATE_HEIGHT: 40,
+    DEFEAT_HEIGHT: 120,
     mergeMap: {
         '엔에이치': '코카콜라',
         '코카콜라': '삼성전자',
@@ -11,19 +13,20 @@ const CONSTANTS = {
         '아마존': '구글',
         '구글': '마이크로소프트',
         '마이크로소프트': '애플',
-        '애플': '나무'
+        '애플': '나무',
+        '나무': '나무'
     },
     ICON_SCORES: {
-        '코카콜라': 2,
+        '코카콜라': 1,
         '삼성전자': 3,
         '테슬라': 6,
-        '메타': 8,
-        '엔비디아': 10,
-        '아마존': 13,
-        '구글': 17,
-        '마이크로소프트': 24,
-        '애플': 27,
-        '나무': 100
+        '메타': 10,
+        '엔비디아': 15,
+        '아마존': 21,
+        '구글': 28,
+        '마이크로소프트': 36,
+        '애플': 45,
+        '나무': 55
     },
     SPAWN_KEYS: ['엔에이치', '코카콜라', '삼성전자']
 };
@@ -53,10 +56,15 @@ function computeGameDimension(dimensionType) {
     return CONSTANTS[dimensionType === 'width' ? 'ORIGINAL_WIDTH' : 'ORIGINAL_HEIGHT'] * scale;
 }
 
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+}
+
 // Game Logic
 let game = new Phaser.Game(CONFIG);
 let currentIcon;
 let icons = [];
+let GLOBAL_SCALE = Math.min(window.innerWidth / CONSTANTS.ORIGINAL_WIDTH, window.innerHeight / CONSTANTS.ORIGINAL_HEIGHT);
 
 function preload() {
     this.load.image('엔에이치', 'stockgame/01.png');
@@ -78,14 +86,16 @@ function create() {
     spawnIcon(this);
     this.input.on('pointerdown', (pointer) => {
         if (currentIcon) {
-            currentIcon.x = pointer.x; // Set the x-coordinate of the icon to the pointer's x-coordinate
+            let clampedX = clamp(pointer.x, currentIcon.radius, this.game.config.width - currentIcon.radius);
+            currentIcon.x = clampedX;
         }
     });
 
     // Pointer Move Event (Drag)
     this.input.on('pointermove', (pointer) => {
-        if (currentIcon && pointer.isDown) { // Check if the pointer is currently pressed down
-            currentIcon.x = pointer.x; // Update the icon's x-coordinate to follow the pointer
+        if (currentIcon && pointer.isDown) {
+            let clampedX = clamp(pointer.x, currentIcon.radius, this.game.config.width - currentIcon.radius);
+            currentIcon.x = clampedX;
         }
     });
 
@@ -94,7 +104,7 @@ function create() {
         if (currentIcon) {
             currentIcon.x = pointer.x; // Update the x-coordinate one last time to ensure accuracy
             currentIcon.setStatic(false);
-            Phaser.Physics.Matter.Matter.Body.setVelocity(currentIcon.body, {x: 0, y: 20});
+            Phaser.Physics.Matter.Matter.Body.setVelocity(currentIcon.body, {x: 0, y: 20*GLOBAL_SCALE});
             currentIcon.setAngularVelocity(0);
             currentIcon.isDropping = true; 
             currentIcon = null;
@@ -111,19 +121,19 @@ function create() {
     });
 
     this.score = 0;
-    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score, { fontSize: '32px', fill: '#fff' });
+    this.scoreText = this.add.text(24, 24, this.score, { fontSize: 32 * GLOBAL_SCALE + 'px', fill: '#fff' });
 }
 
 function update() {
+    console.log('DF_h:' + CONSTANTS.DEFEAT_HEIGHT, 'CR_h:' + CONSTANTS.CREATE_HEIGHT)
     for (let i = 0; i < icons.length; i++) {
         // Check for defeat condition
-        if (!icons[i].isStatic && !icons[i].isDropping && icons[i].y <= 40) {
+        if (!icons[i].isStatic && !icons[i].isDropping && icons[i].y <= CONSTANTS.DEFEAT_HEIGHT * GLOBAL_SCALE) {
             endGame('defeat');
             return; // Exit the update function once a defeat is detected
         }
     }
 }
-
 
 function handleMerge(icon1, icon2) {
 
@@ -133,7 +143,7 @@ function handleMerge(icon1, icon2) {
         let nextIconKey = CONSTANTS.mergeMap[icon1.texture.key];
         if (nextIconKey) {
             let newIcon = this.matter.add.image((icon1.x + icon2.x) / 2, (icon1.y + icon2.y) / 2, nextIconKey, null, { restitution: 0.5, friction: 0.05 });
-            newIcon.setScale(0.5, 0.5);
+            newIcon.setScale(0.5 * GLOBAL_SCALE, 0.5 * GLOBAL_SCALE);
             let scaledRadius = (newIcon.width * newIcon.scaleX) / 2;
             newIcon.setCircle(scaledRadius);
             newIcon.setStatic(false); 
@@ -155,11 +165,9 @@ function handleMerge(icon1, icon2) {
         icons = icons.filter(icon => icon !== icon1 && icon !== icon2);
 
         this.score += CONSTANTS.ICON_SCORES[nextIconKey];
-        this.scoreText.setText('Score: ' + this.score); // Update the score display
+        this.scoreText.setText(this.score); // Update the score display
     }
 }
-
-
 
 function spawnIcon(scene) {
     let randomIconKey = Phaser.Math.RND.pick(CONSTANTS.SPAWN_KEYS);
@@ -168,12 +176,14 @@ function spawnIcon(scene) {
 }
 
 function createIcon(scene, iconKey) {
-    let icon = scene.matter.add.image(computeGameDimension('width') / 2, 20, iconKey);
-    icon.setScale(0.5, 0.5);
+    let icon = scene.matter.add.image(computeGameDimension('width') / 2, CONSTANTS.CREATE_HEIGHT * GLOBAL_SCALE, iconKey);
+    icon.setScale(0.5 * GLOBAL_SCALE, 0.5 * GLOBAL_SCALE);
+    icon.setInteractive(); // Ensure the icon is interactive
     let radius = (icon.width * icon.scaleX) / 2;
     icon.setCircle(radius);
     icon.setStatic(true);
     icon.isDropping = false;
+    icon.radius = radius;  // Store the radius in the icon object for easy access later
     return icon;
 }
 
